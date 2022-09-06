0757087a399b9ae9df2a871bbeb16ec0bf546adb
Djordje Knezevic <starwarrior@mycity.rs>
2022-09-06 18:03:09 +0200

    Randomize 8 Ball image when Ask button is pressed and finish project

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -11,14 +11,13 @@ import UIKit
 class ViewController: UIViewController {
     
     let ballArray = [#imageLiteral(resourceName: "ball1"), #imageLiteral(resourceName: "ball2"), #imageLiteral(resourceName: "ball3"), #imageLiteral(resourceName: "ball5"), #imageLiteral(resourceName: "ball4")]
-
+    
     @IBOutlet weak var imageView: UIImageView!
     
-    
     @IBAction func askButtonPressed(_ sender: UIButton) {
-        imageView.image = ballArray[2]
+        imageView.image = ballArray[Int.random(in: 0...4)]
     }
     
-    
+
 }
 
